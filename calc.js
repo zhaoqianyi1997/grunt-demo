e85a60a5757c82b904947519e5b078a3204f3bca
zhaoqianyi1997 <495225376@qq.com>
2019-03-19 08:46:59 +0800

    sprite

---
+++ calc.js
@@ -0,0 +1,16 @@
+
+n reactangle(){
+  return{
+    'perimeter':function(width,height){
+            return s*(Number(width)+Number(height));
+                
+    },
+
+    'area':function(width,height){
+            return Number(width)*Number(height);
+                
+    }
+      
+  }
+
+}
