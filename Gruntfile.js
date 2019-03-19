a2328c6ef790df6fb05abc920ea1c5c1d1401752
zhaoqianyi1997 <495225376@qq.com>
2019-03-19 08:37:51 +0800

    htmlmin

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+     
+    htmlmin: {    
+      dev:{
+        options:{
+          collapseWhitespace:true,
+          preserveLineBreaks:false
+        },
+      
+      files: {
+           'dist/index.html': './index.html'
+          
+      }
+    }
+    
+   }
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+      grunt.registerTask('default', ['htmlmin']);
+
+};
+
+                            
+
+  
